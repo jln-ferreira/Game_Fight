24f0cc9a5f546dffc1721cd0b8ecc382776211a2
jln-ferreira <jln-ferreira@hotmail.com>
2020-01-22 15:46:33 -0800

    show first choice ok- to do: fix css and choose a hero

--- Code_Game/CharOOP.php
+++ Code_Game/CharOOP.php
@@ -105,14 +105,36 @@ class Hero extends Char{
 
     //methods HERO -->
     function status(){
-        echo "The $this->Name : <br>
-            HP: $this->HP <br>
-            STR: $this->STR <br>
-            DEF: $this->DEF <br>
-            AGI: $this->AGI <br>
-            MANA: $this->MANA <br>
-            EXP: $this->EXP <br>";
-    }
+        return "<table class='table table-sm'>
+                  <tr>
+                    <th colspan='2'>$this->Name</th>
+                  </tr>
+                  <tr>
+                    <td>HP</td>
+                    <td>$this->HP</td>
+                  </tr>
+                    <tr>
+                    <td>STR</td>
+                    <td>$this->STR</td>
+                  </tr>
+                    <tr>
+                    <td>DEF</td>
+                    <td>$this->DEF</td>
+                  </tr>
+                    <tr>
+                    <td>AGI</td>
+                    <td>$this->AGI</td>
+                  </tr>
+                    <tr>
+                    <td>MANA</td>
+                    <td>$this->MANA</td>
+                  </tr>
+                    <tr>
+                    <td>EXP</td>
+                    <td>$this->EXP</td>
+                  </tr>
+             </table>";
+    }    
 }
 
 
--- Code_Game/GameBattle.php
+++ Code_Game/GameBattle.php
@@ -1,3 +1,5 @@
+<style src="css/styles_Cards.css" type="text/css"></style>
+
 <?php
 //bring Header to MainPage
 require_once "Header.html";
@@ -23,8 +25,8 @@ require_once "Iniciate_Bag.php";
 
 		<div class="col-xs-6 col-md-4">
 			<div class="card" style="width: 18rem;">
-				<img id="startGame" src="../image/2WarriorWalk.gif"> <!-- Warrior GIF -->
-				<div class="card-body">
+				<img src="../image/2WarriorWalk.gif"> <!-- Warrior GIF -->
+				<div class="card-body" style="padding-bottom: 2px">
 					<h5 class="card-title">Warrior</h5>
 					<p class="card-text"> <?php echo $arrHEROS[0][0]->status(); ?></p>
 				</div>
@@ -33,7 +35,7 @@ require_once "Iniciate_Bag.php";
 
 		<div class="col-xs-6 col-md-4">
 			<div class="card" style="width: 18rem;">
-				<img id="startGame" src="../image/6ArcherWalk.gif"> <!-- Archer GIF -->
+				<img src="../image/6ArcherWalk.gif"> <!-- Archer GIF -->
 				<div class="card-body">
 					<h5 class="card-title">Archer</h5>
 					<p class="card-text"><?php echo $arrHEROS[1][0]->status(); ?></p>
@@ -43,13 +45,34 @@ require_once "Iniciate_Bag.php";
 
 		<div class="col-xs-6 col-md-4">
 			<div class="card" style="width: 18rem;">
-				<img id="startGame" src="../image/5MageWalk.gif"> <!-- Mage GIF -->
+				<img src="../image/5MageWalk.gif"> <!-- Mage GIF -->
 				<div class="card-body">
 					<h5 class="card-title">Wizzard</h5>
 					<p class="card-text"><?php echo $arrHEROS[2][0]->status(); ?></p>
 				</div>
 			</div>
 		</div>
+
+
+	<?php 
+
+		$LenghHeroArr = sizeof($arrHEROS); // 3
+
+		for ($i=0; $i < $LenghHeroArr; $i++) { 
+			echo "<div class='col-xs-6 col-md-4'>
+					<div class='card' style='width: 18rem;'>
+						<img src='../image/ShowHero/show" . $i . ".gif'> 
+						<div class='card-body' style='padding-bottom: 2px'>
+							<h5 class='card-title'>Warrior</h5>
+							<p class='card-text'>" . $arrHEROS[$i][0]->status() . "</p>
+						</div>
+					</div>
+				</div>";
+
+		}
+
+
+	?>
 		
 
 	</div> 
